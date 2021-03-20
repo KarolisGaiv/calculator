28f97ba73abb7173ca3d61d36aefb10adbe15657
KarolisGaiv <kgaivenis@gmail.com>
2021-03-20 17:21:42 +0200

    Add operator functions, buttons for numbers and operators

---
+++ script.js
@@ -0,0 +1,21 @@
+// ----------  Functions -----------
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  return operator(num1, num2);
+}
